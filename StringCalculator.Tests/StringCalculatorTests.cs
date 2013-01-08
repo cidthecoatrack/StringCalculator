52009481385850b1b771010f87fc7f15ab4149d3
Karl Speer <cidthecoatrack@gmail.com>
2013-01-08 13:45:46 -0500

    adding repository

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorKata;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        StringCalculator Calculator;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            Calculator = new StringCalculator();
+        }
+        
+        [TestMethod]
+        public void EmptyString_Zero()
+        {
+            Assert.AreEqual(0, Calculator.Add(""));
+        }
+
+        [TestMethod]
+        public void OneNumber_Itself()
+        {
+            Assert.AreEqual(1, Calculator.Add("1"));
+        }
+
+        [TestMethod]
+        public void TwoNumbers_TheSum()
+        {
+            Assert.AreEqual(3, Calculator.Add("1,2"));
+        }
+
+        [TestMethod]
+        public void FiveNumbers_TheSum()
+        {
+            Assert.AreEqual(25, Calculator.Add("5,5,5,5,5"));
+        }
+
+        [TestMethod]
+        public void NewLineAsDelimiter_StillSumAddends()
+        {
+            Assert.AreEqual(6, Calculator.Add("1\n2,3"));
+        }
+
+        [TestMethod]
+        public void CreatingCustomDelimiter_AllowDelimiterInAddendsString()
+        {
+            Assert.AreEqual(3, Calculator.Add("//;\n1,2"));
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void NegativeNumbers_ThrowExceptionNegativesNotAllowed()
+        {
+            Calculator.Add("-1");
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void InvalidNumbers_ShouldBeIncludedInException()
+        {
+            try
+            {
+                Calculator.Add("1,-2,-7");
+            }
+            catch (ArgumentException exception)
+            {
+                Assert.IsTrue(exception.Message.Contains("-2"));
+                Assert.IsTrue(exception.Message.Contains("-7"));
+                throw new ArgumentException();
+            }
+        }
+
+        [TestMethod]
+        public void DashAsDelimiter_ShouldWorkFine()
+        {
+            Assert.AreEqual(6, Calculator.Add("//-\n1-2-3"));
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string Addends)
+        {
+            if (String.IsNullOrEmpty(Addends)) return 0;
+            ValidateAddends(Addends);
+
+            int Sum = 0;
+            foreach (int addend in ParseAddends(Addends, GetCustomDelimiter(ref Addends)))
+                Sum += addend;
+
+            return Sum;
+        }
+
+        private List<int> ParseAddends(string Addends, char CustomDelimiter)
+        {
+            string[] addendsStringArray = Addends.Split(',', '\n', CustomDelimiter);
+            List<int> ParsedAddends = new List<int>();
+            
+            foreach (string StringAddend in addendsStringArray)
+                ParsedAddends.Add(Int32.Parse(StringAddend));
+
+            return ParsedAddends;
+        }
+
+        private static char GetCustomDelimiter(ref string Addends)
+        {
+            char CustomDelimiter = ',';
+            if (Addends.StartsWith("//"))
+            {
+                CustomDelimiter = Addends[2];
+                Addends = Addends.Remove(0, 4);
+            }
+            return CustomDelimiter;
+        }
+
+        private void ValidateAddends(string Addends)
+        {
+            char CustomDelimiter = GetCustomDelimiter(ref Addends);
+            string[] AddendArray = Addends.Split(',', '\n', CustomDelimiter);
+            string message = "Negatives not allowed: ";
+            bool FoundInvalidNumber = false;
+            foreach (string AddendString in AddendArray)
+            {
+                if (Int32.Parse(AddendString) < 0)
+                {
+                    message += " " + Int32.Parse(AddendString);
+                    FoundInvalidNumber = true;
+                }
+            }
+
+            if (FoundInvalidNumber)
+                throw new ArgumentException(message);
+        }
+    }
+}
